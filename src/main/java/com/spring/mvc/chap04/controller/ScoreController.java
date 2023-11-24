72cae7556f14193b03b818f5ee5b5a2b5e10fb4b
SeungHanLee <swq3064@naver.com>
2023-11-24 09:40:17 +0900

    #1124 1교시

--- src/main/java/com/spring/mvc/chap04/controller/ScoreController.java
+++ src/main/java/com/spring/mvc/chap04/controller/ScoreController.java
@@ -1,9 +1,17 @@
 package com.spring.mvc.chap04.controller;
 
+import com.spring.mvc.chap04.entity.Score;
+import com.spring.mvc.chap04.repository.ScoreRepository;
+import com.spring.mvc.chap04.repository.ScoreRepositoryImpl;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /*
     # 컨트롤러
@@ -26,13 +34,25 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/score")
+@RequiredArgsConstructor // final이 붙은 필드를 초기화하는 생성자를 생성
+//@AllArgsConstructor // 모든 필드를 초기화하는 생성자를 생성해준다.
 public class ScoreController {
+    // 저장소에 의존하여 데이터 처리를 위임한다.
+    // 의존객체는 불변성을 가지는 것이 좋다.
+    private final ScoreRepository repository;
+
+//    @Autowired // 생성자 주입을 사용하고 생성자가 단 하나 -> Autowired 생략가능
+//    public ScoreController(ScoreRepository repository) {
+//        this.repository = repository;
+//    }
 
     // 1. 성적 등록 폼 띄우기 + 목록 조회 요청
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
         System.out.println("/score/list GET");
-        return "";
+        List<Score> scoreList = repository.findAll();
+        model.addAttribute("sList", scoreList);
+        return "chap04/score-list";
     }
 
     // 2. 성적정보를 데이터베이스에 저장하는 요청
