0a4493e97a77da307a58c9cef9dcca0dcec1c0cc
SeungHanLee <swq3064@naver.com>
2023-12-26 09:48:33 +0900

    #1226 1교시

--- src/main/java/com/spring/mvc/chap05/controller/MemberController.java
+++ src/main/java/com/spring/mvc/chap05/controller/MemberController.java
@@ -1,16 +1,20 @@
 package com.spring.mvc.chap05.controller;
 
+import com.spring.mvc.chap05.dto.request.LoginRequestDTO;
 import com.spring.mvc.chap05.dto.request.SignUpRequestDTO;
+import com.spring.mvc.chap05.service.LoginResult;
 import com.spring.mvc.chap05.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/members")
@@ -47,4 +51,29 @@ public class MemberController {
         return join ? "redirect:/board/list" : "redirect:/members/sign-up";
     }
 
+    // 로그인 양식 요청
+    @GetMapping("/sign-in")
+    public String signIn() {
+        log.info("/members/sign-in GET = forwarding to sign-in.jsp");
+
+        return "members/sign-in";
+    }
+
+    // 로그인 검증 요청
+    @PostMapping("/sign-in")
+    public String signIn(LoginRequestDTO dto,
+                         // 모델에 담긴 데이터는 redirect시 jsp로 가지 않는다
+                         // redirect는 요청이 2번 들가기 때문에 첫번째 요청시 model에 보관된 데이터가 소실된다
+                         RedirectAttributes ra) {
+        log.info("/members/sign-in POST!");
+        log.debug("param: {}", dto);
+        LoginResult loginResult = memberService.authenticate(dto);
+        log.debug("login result: {}", loginResult);
+
+        ra.addFlashAttribute("msg", loginResult);
+        if(loginResult.equals(LoginResult.SUCCESS)){
+            return "redirect:/board/list";
+        }
+        return "redirect:/members/sign-in";
+    }
 }
--- src/main/java/com/spring/mvc/chap05/dto/request/LoginRequestDTO.java
+++ src/main/java/com/spring/mvc/chap05/dto/request/LoginRequestDTO.java
@@ -3,6 +3,7 @@ package com.spring.mvc.chap05.dto.request;
 import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @ToString
 @EqualsAndHashCode
